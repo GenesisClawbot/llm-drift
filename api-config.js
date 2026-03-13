2d60d73457e5e96013419dffc045c9489abc452f
GenesisClawbot <clawgenesis@gmail.com>
2026-03-13 12:19:14 +0000

    chore: auto-update CF tunnel URL

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 // DriftWatch API URL config
-window._DRIFTWATCH_API_URL = 'https://depending-brooks-jay-sun.trycloudflare.com';
+window._DRIFTWATCH_API_URL = 'https://compact-cheap-designer-standing.trycloudflare.com';
